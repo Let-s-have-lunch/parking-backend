e5009d6fc8b71991b73471964c10ef66a1c695b4
DeveloperSky95 <haneulgim95@gmail.com>
2026-08-31 14:18:01 +0900

    작성된 JwtFilter와 JwtProvider를 SecurityConfig에 등록

--- src/main/java/com/lineacademy/parkingbackend/config/SecurityConfig.java
+++ src/main/java/com/lineacademy/parkingbackend/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.lineacademy.parkingbackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,27 +27,27 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http, JwtAuthenticationWebFilter jwtAuthenticationWebFilter) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
                 .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors(Customizer.withDefaults())
                 .authorizeExchange(exchanges -> exchanges
                         // 인증 없이 접근 가능한 엔드포인트
                         .pathMatchers(HttpMethod.POST, "/auth/login", "/auth/signup").permitAll()
-                        .pathMatchers(HttpMethod.GET, "/parking-lots/**").permitAll() // 주차장 조회는 일단 누구나 가능하게
+                        .pathMatchers(HttpMethod.GET, "/parking-lots", "/parking-lots/**").permitAll()
                         // 나머지는 인증 필요
                         .anyExchange().authenticated()
                 )
-                // TODO: 여기에 커스텀 JWT Authentication WebFilter 추가 예정
+                .addFilterAt(jwtAuthenticationWebFilter, SecurityWebFiltersOrder.AUTHENTICATION)
                 .build();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Vite(React) 기본 포트 허용
+        configuration.setAllowedOrigins(List.of("http://localhost:8081"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
